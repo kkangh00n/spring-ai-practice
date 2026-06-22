f2724ce9df7c7c382114d263f38d7ea48fa62a13
kkangh00n <kig2454@gmail.com>
2026-06-22 17:23:48 +0900

    Stream 형식으로 응답받기

--- src/main/kotlin/com/kgh/chat/ChatController.kt
+++ src/main/kotlin/com/kgh/chat/ChatController.kt
@@ -2,8 +2,10 @@ package com.kgh.chat
 
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.model.ChatResponse
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 
 @RestController
 class ChatController(
@@ -20,6 +22,14 @@ class ChatController(
             .content() // 0. 받아온 응답 중 메타데이터는 버리고, 순수 content만 추출!
 //            .chatResponse() // 1. ChatResponse 객체로 받기
 //            .entity() // 2. Entity로 받기
+    }
+
+    @GetMapping("/streaming", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun stream(userPrompt: String): Flux<String> {
+        return chatClient.prompt()
+            .user(userPrompt)
+            .stream()
+            .content()
     }
 
 }
